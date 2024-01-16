562116c6d063423c45e431e6468188f677669796
Leonardo <leonardosanchez119@gmail.com>
2024-01-16 12:31:37 -0300

    eliminacion de todos los componentes anteriormente creados para repaso de todo los temas e integracion de bootstrap al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PadreComponent } from './padre/padre.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HijoComponent } from './hijo/hijo.component';
+
 import { FormsModule } from '@angular/forms';
-import { HermanoComponent } from './hermano/hermano.component';
-import { EstiloHermanosDirective } from './estilo-hermanos.directive';
-import { MiPipePipe } from './mi-pipe.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PadreComponent,
-    ContadorComponent,
-    HijoComponent,
-    HermanoComponent,
-    EstiloHermanosDirective,
-    MiPipePipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/mi-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MiPipePipe } from './mi-pipe.pipe';
-
-describe('MiPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MiPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
