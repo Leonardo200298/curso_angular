db4b926b876b1428d0de7e50a3798a1aea3a7612
Leonardo200298 <leonardosanchez119@gmail.com>
2024-01-31 15:55:25 -0300

    funcion de get con servicio httpClient echa, falta subscripcion al observable y mostrar consulta en el template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -24,8 +24,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule, //formularios de tipo plantilla
-    ReactiveFormsModule //formularios de tipo reactivo
-
+    ReactiveFormsModule, //formularios de tipo reactivo
+    HttpClientModule// para hacer peticiones http
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
-  constructor() { }
+  private baseURL = 'https://fakestoreapi.com/products';
+
+  constructor(private _httpClient: HttpClient) { }
+
+  public getProducts():Observable<IProduct[]>{
+    return this._httpClient.get<IProduct[]>(this.baseURL);
+  }
 }
